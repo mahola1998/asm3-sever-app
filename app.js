96de5f25a88549c6c3829c988aa6e5fa0dc3bd34
mahola1998 <77270158+mahola1998@users.noreply.github.com>
2024-11-11 01:24:20 +0700

    Fix port binding for Render deployment

--- app.js
+++ app.js
@@ -141,7 +141,7 @@ io.on("connection", (socket) => {
 });
 
 mongoose
-  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGODB_URI)
   .then(() => server.listen(process.env.PORT || 5000))
   .catch((err) => {
     console.error("Database connection error:", err);
