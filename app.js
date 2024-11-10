eb4952e9380298eee8e83dfa59c8e54ce3470290
mahola1998 <77270158+mahola1998@users.noreply.github.com>
2024-11-11 00:29:25 +0700

    Initial commit

--- app.js
+++ app.js
@@ -141,11 +141,7 @@ io.on("connection", (socket) => {
 });
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    ssl: true,
-  })
+  .connect(MONGODB_URI)
   .then(() => server.listen(process.env.PORT || 5000))
   .catch((err) => {
     console.error("Database connection error:", err);
